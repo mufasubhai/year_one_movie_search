1584943c52d007213e32cb899d9fd74138190ea7
Adrian Apodaca <aapodaca@gmail.com>
2020-12-14 20:52:54 -0800

    update Movie schema

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const keys = require('../../config/keys');
 const Movie = require('../../models/Movie')
 
+const validateMovieInput = require('../../validation/movies');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the movies route" }));
 
-const validateMovieInput = require('../../validation/movies');
 
 router.get('/', (req, res) => {
     Movie.find()
@@ -16,7 +16,50 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/:id', (req, res) => {
+    Movie.findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err =>
+            res.status(404).json({nomoviesfound: "No movie found with that ID"})
+        );
+})
 
+
+router.post('/',
+    (req, res) => {
+      const { errors, isValid } = validateMovieInput(req.body);
+  
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+  
+      const newMovie = new Movie({
+        title: req.body.title,
+        upVote: req.body.upVotes,
+        downVote: req.body.downVote
+      });
+  
+      newMovie.save().then(movie => res.json(movie));
+    }
+  );
+
+
+
+    router.patch(
+  "/:id", (req, res) => {
+    const { downVote, upVote } = req.body;
+
+    Movie.findById(req.params.id).then((movie) => {
+        movie.downVote = downVote;
+      movie.upVote = upVote;
+
+      movie.save()
+        .then((savedMovie) => res.json(savedMovie))
+        .catch((err) => res.json(err));
+    });
+    return res;
+  }
+);
 
 
 module.exports = router;
