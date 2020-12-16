1565b6bed609059776849eff38a89c5f0b73b039
Adrian Apodaca <aapodaca@gmail.com>
2020-12-15 21:39:44 -0800

    update and add ejs for package

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const db = require('./config/keys').mongoURI;
 const movies = require("./routes/api/movies");
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const index = "./index.html"
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
+app.engine('html', require('ejs').renderFile);
 // mongoDB connect
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,9 +20,9 @@ mongoose
 // movies route 
 app.use('/api/movies', movies)
 
-
-app.get("/", (req, res) => res.send("Hello World"));
-
+app.get('/', function(req, res) {
+  res.render('index.html')
+})
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
